ee4d67ef34bd01a4f6876a1560372495543859f2
tyakovenko <taisia.yakovenko@gmail.com>
2023-11-09 17:50:47 -0500

    edit config after fixing merge conflicts

--- src/main.go
+++ src/main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"BGPAlert/analyzing"
 	"BGPAlert/common"
+	"BGPAlert/config"
 	"BGPAlert/parsing"
 	"BGPAlert/processing"
+	"log"
 	"sync"
 )
 
 func main() {
 
-	/*
-		config, err := loadConfig("config.json")
-		if err != nil {
-			log.Fatal("Error loading configuration:", err)
-		}
-		validDateConfiguration(config)
-	*/
+	configStruct, err := config.LoadConfig("config.json")
+	if err != nil {
+		log.Fatal("Error loading configuration:", err)
+	}
+	config.ValidDateConfiguration(configStruct)
 
 	// WaitGroup for waiting on goroutines to finish
 	var wg sync.WaitGroup
