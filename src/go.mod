3fbf0b9fcb72e856eaa9d58049471c1cd176d38d
Brendan <47164233+bman4@users.noreply.github.com>
2023-11-09 14:31:01 -0800

    Implemented script to download static data

--- src/go.mod
+++ src/go.mod
@@ -2,19 +2,6 @@ module BGPAlert
 
 go 1.19
 
-require gonum.org/v1/plot v0.14.0
+require github.com/gorilla/websocket v1.5.1
 
-require (
-	git.sr.ht/~sbinet/gg v0.5.0 // indirect
-	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
-	github.com/campoy/embedmd v1.0.0 // indirect
-	github.com/go-fonts/liberation v0.3.1 // indirect
-	github.com/go-latex/latex v0.0.0-20230307184459-12ec69307ad9 // indirect
-	github.com/go-pdf/fpdf v0.8.0 // indirect
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/gorilla/websocket v1.5.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/image v0.11.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-)
+require golang.org/x/net v0.17.0 // indirect
---
+++ src/static_data/get_static_data.go
@@ -0,0 +1,236 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type Folder struct {
+	FolderName string
+	Urls       []string
+}
+
+func downloadFolder(folder Folder) error {
+	// Create the folder on the file system
+	err := os.MkdirAll(folder.FolderName, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	// Download files to the folder
+	for _, url := range folder.Urls {
+		err := downloadFile(url, folder.FolderName)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func downloadFile(url, folderDir string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("HTTP request failed with status code %d", resp.StatusCode)
+	}
+
+	// Extract the filename from the URL
+	_, filename := filepath.Split(url)
+
+	// Create the file in the specified folder
+	filePath := filepath.Join(folderDir, filename)
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Copy the file from the response body to the local file
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Downloaded: %s\n", filePath)
+	return nil
+}
+
+func main() {
+	// Create folder instances
+	bgptest1 := Folder{
+		FolderName: "bgptest1",
+		Urls: []string{
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1845.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1900.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1915.bz2",
+		},
+	}
+
+	bgptest2 := Folder{
+		FolderName: "bgptest2",
+		Urls: []string{
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1545.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1600.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1615.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1630.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1645.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1700.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1715.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1730.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1745.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1800.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1815.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1830.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1845.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1900.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211204.1915.bz2",
+		},
+	}
+
+	bgptest3 := Folder{
+		FolderName: "bgptest3",
+		Urls: []string{
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211209.1530.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211209.1545.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211209.1600.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211209.1615.bz2",
+		},
+	}
+
+	bgptest4 := Folder{
+		FolderName: "bgptest4",
+		Urls: []string{
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1615.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1630.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1645.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1700.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1715.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1730.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1745.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1800.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1815.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1830.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1845.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1900.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1915.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1930.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.1945.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2000.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2015.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2030.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2045.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2100.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2115.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2130.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2145.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2200.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2215.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2230.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2245.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2300.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2315.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211220.2330.bz2",
+		},
+	}
+
+	bgptest5 := Folder{
+		FolderName: "bgptest5",
+		Urls: []string{
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.1800.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.1815.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.1830.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.1845.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.1900.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.1915.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.1930.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.1945.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2000.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2015.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2030.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2045.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2100.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2115.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2130.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2145.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2200.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2215.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2230.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2245.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2300.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2315.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2330.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211228.2345.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211229.0000.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211229.0015.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211229.0030.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211229.0045.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211229.0100.bz2",
+		},
+	}
+
+	bgpnyfiles := Folder{
+		FolderName: "bgpnyfiles",
+		Urls: []string{
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211201.0000.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211201.0015.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211201.0030.bz2",
+			"http://routeviews.org/route-views.ny/bgpdata/2021.12/UPDATES/updates.20211201.0045.bz2",
+		},
+	}
+
+	bgpupdatefiles := Folder{
+		FolderName: "bgpupdatefiles",
+		Urls: []string{
+			"http://routeviews.org/route-views.chicago/bgpdata/2016.06/UPDATES/updates.20160629.1330.bz2",
+			"http://routeviews.org/route-views.chicago/bgpdata/2016.06/UPDATES/updates.20160629.1345.bz2",
+			"http://routeviews.org/route-views.chicago/bgpdata/2016.06/UPDATES/updates.20160629.1400.bz2",
+			"http://routeviews.org/route-views.chicago/bgpdata/2016.06/UPDATES/updates.20160629.1415.bz2",
+			"http://routeviews.org/route-views.chicago/bgpdata/2016.06/UPDATES/updates.20160629.1430.bz2",
+			"http://routeviews.org/route-views.chicago/bgpdata/2016.06/UPDATES/updates.20160629.1445.bz2",
+		},
+	}
+
+	// Download folders
+	err := downloadFolder(bgptest1)
+	if err != nil {
+		fmt.Println("Error downloading folder bgptest1:", err)
+	}
+
+	err = downloadFolder(bgptest2)
+	if err != nil {
+		fmt.Println("Error downloading folder bgptest2:", err)
+	}
+
+	err = downloadFolder(bgptest3)
+	if err != nil {
+		fmt.Println("Error downloading folder bgptest3:", err)
+	}
+
+	err = downloadFolder(bgptest4)
+	if err != nil {
+		fmt.Println("Error downloading folder bgptest4:", err)
+	}
+
+	err = downloadFolder(bgptest5)
+	if err != nil {
+		fmt.Println("Error downloading folder bgptest5:", err)
+	}
+
+	err = downloadFolder(bgpnyfiles)
+	if err != nil {
+		fmt.Println("Error downloading folder bgpnyfiles:", err)
+	}
+
+	err = downloadFolder(bgpupdatefiles)
+	if err != nil {
+		fmt.Println("Error downloading folder bgpupdatefiles:", err)
+	}
+}
