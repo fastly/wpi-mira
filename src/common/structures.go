536a357fde5ff4b256c6dc16b248b8476ad602d5
bman4 <bmannion662@gmail.com>
2023-11-13 19:54:20 +0000

    Made address and prefix stronger types

--- src/common/structures.go
+++ src/common/structures.go
@@ -1,15 +1,16 @@
 package common
 
 import (
+	"net/netip"
 	"time"
 )
 
 type BGPMessage struct {
 	Timestamp      time.Time
 	BGPMessageType string
-	PeerIP         string
+	PeerIP         netip.Addr
 	PeerASN        uint32
-	Prefix         string
+	Prefix         netip.Prefix
 }
 
 type Window struct {
