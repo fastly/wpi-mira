0939e1c72dfbf940a1d89e8270e93de8dab96dcb
tyakovenko <taisia.yakovenko@gmail.com>
2023-11-21 19:12:10 -0500

    added optimize package with one file which calculates the minimum tau to get the minimum output

--- src/blt_mad/bltMad.go
+++ src/blt_mad/bltMad.go
@@ -4,12 +4,12 @@ import (
 	"math"
 )
 
-//mad
-func mad(data []float64) float64 {
+//Mad
+func Mad(data []float64) float64 {
 	//implement MAD given the list of the number of messages in each given bucket
 	sum := 0.0
 	if len(data) == 0 {
-		return math.SmallestNonzeroFloat64 //invalid mad in terms of BGP message counts
+		return math.SmallestNonzeroFloat64 //invalid Mad in terms of BGP message counts
 	} else {
 		for _, value := range data {
 			sum += math.Abs(value - findMean(data))
@@ -19,7 +19,7 @@ func mad(data []float64) float64 {
 	return result
 }
 
-//blt mad
+//blt Mad
 
 func BltMad(data []float64, tau float64) []float64 {
 	var outliers []float64
@@ -29,8 +29,8 @@ func BltMad(data []float64, tau float64) []float64 {
 	} else {
 		//calculate blt formula
 		//everything is based on the noZeroData since we are looking at spikes rather than lack of messages
-		med := findMedian(noZeroData)
-		m := mad(noZeroData)
+		med := FindMedian(noZeroData)
+		m := Mad(noZeroData)
 
 		bltScore := math.Abs(med - tau*m)
 		for _, value := range noZeroData {
